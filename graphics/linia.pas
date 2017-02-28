d5d56498a83857acf64b1364127932047cb05d1f
Andrey Rybak <rybak.a.v@gmail.com>
2017-03-01 02:36:25 +0300

    linia.pas: improve color changing on key presses

--- graphics/linia.pas
+++ graphics/linia.pas
@@ -3,6 +3,7 @@ uses ptcgraph, ptccrt;
 const
 	recolorCount = 100;
 	delayTime = 10;
+	colorInc : array [0..4] of integer = (1, 10, 100, 1000, 5000);
 var
 	gDriver, gMode : integer;
 
@@ -42,16 +43,20 @@ repeat
 	x1 := x2;
 	y1 := y2;
 	if keypressed then
+	begin
 		ch := readkey;
-	if ch = 'a' then
-	begin
-		c := c + 1;
-		setColor(c);
-	end;
-	if ch = #13 then
-	begin
-		c := 0;
-		setColor(0);
+		if ch in ['a'..'e'] then
+		begin
+			c := c + colorInc[ ord(ch) - ord('a') ];
+			setColor(c);
+			count := 0;
+		end;
+		if ch = #13 then
+		begin
+			c := 0;
+			setColor(0);
+			count := 0;
+		end;
 	end;
 until ch = #32;
 closeGraph;
